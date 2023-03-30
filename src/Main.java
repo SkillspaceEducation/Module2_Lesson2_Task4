b010fc3476d15a294aa4d5bae4fdea74da493fff
Tagir Murtazin <tr.murtazin@yandex.ru>
2023-03-30 14:28:35 +0300

    Даны строки разной длины (длина - четное число), необходимо вернуть ее два средних знака. Например, если дана строка "string" результат будет "ri", для строки "code" результат "od", для "Practice" результат "ct".

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
+/*
+Даны строки разной длины (длина - четное число), необходимо вернуть ее два средних знака.
+Например, если дана строка "string" результат будет "ri", для строки "code" результат "od",
+для "Practice" результат "ct".
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        char[] str2 = new char[2];
+        char[] c = str.toCharArray();
+        System.out.println(c[c.length / 2 - 1] + "" + c[c.length / 2]);
+        System.out.println(str.substring(str.length() / 2 - 1, str.length() / 2 + 1));
+        str.getChars(str.length() / 2 - 1, str.length() / 2 + 1, str2, 0);
+        System.out.println(str2);
     }
 }
